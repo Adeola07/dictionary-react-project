20bac08f3e2c1f220dee9efdb972528f0caa7e93
Adeola07 <88549439+Adeola07@users.noreply.github.com>
2022-04-09 16:06:11 +0100

    Added footer

--- src/App.js
+++ src/App.js
@@ -9,6 +9,23 @@ function App() {
       <div className="container">
         <Dictionary defaultKeyword="waterfall" />
       </div>
+      <footer>
+        <a
+          href="https://github.com/Adeola07/dictionary-react-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open source code
+        </a>{" "}
+        by{" "}
+        <a
+          href="https://laughing-boyd-4e1917.netlify.app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Adeola Ejalonibu
+        </a>
+      </footer>
     </div>
   );
 }
