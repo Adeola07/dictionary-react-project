1007ab3991d6cc0463d203c17470be5ae069d357
Adeola07 <88549439+Adeola07@users.noreply.github.com>
2022-04-09 10:29:04 +0100

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -7,6 +7,7 @@ import Result from "./Result";
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
   const [result, setResult] = useState(null);
+  const [loaded, setLoaded] = useState(false);
 
   function handleKeyword(event) {
     setKeyword(event.target.value);
@@ -14,37 +15,46 @@ export default function Dictionary() {
   function handleResponse(response) {
     setResult(response.data[0]);
   }
-  function search(event) {
+  function handleSubmit(event) {
     event.preventDefault();
-
+    search();
+  }
+  function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
-  return (
-    <div className="Dictionary">
-      <form onSubmit={search}>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              onChange={handleKeyword}
-              className="form-control"
-              placeholder="Enter word here..."
-              autoFocus="on"
-            />
+  function load() {
+    setLoaded(true);
+  }
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                onChange={handleKeyword}
+                className="form-control"
+                placeholder="Enter word here..."
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-50"
+              />
+            </div>
           </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-50"
-            />
-          </div>
-        </div>
-      </form>
+        </form>
 
-      <Result result={result} />
-    </div>
-  );
+        <Result result={result} />
+      </div>
+    );
+  } else {
+    return load();
+  }
 }
